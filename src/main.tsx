56bd1675369e9eaa0bd6696fc90b207294db055b
amajewski1994 <amajewski1994@gmail.com>
2026-04-22 16:09:41 +0200

    add smooth scroll

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ReactLenis } from "lenis/react"
+import "lenis/dist/lenis.css"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReactLenis
+      root
+      options={{
+        duration: 1.1,
+        smoothWheel: true,
+      }}
+    >
+      <App />
+    </ReactLenis>
   </StrictMode>,
 )
